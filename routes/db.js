55061997fdf78942ceb9685055816ec325b99208
hunter <hunter@hunter.hunter.xyx>
2024-07-10 23:50:04 -0300

    agrego conexion a base de datos y rutas

---
+++ routes/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'ricardo',
+  password: '1234ricardo*',
+  database: 'nescac'
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database as id ' + connection.threadId);
+});
+
+module.exports = connection;
--- routes/routesapp.js
+++ routes/routesapp.js
@@ -1,36 +1,62 @@
 const express = require('express');
-//const router = express.Router();
 const defaultRoute = express.Router();
+const connection = require('./db');
 
 defaultRoute.get('/index', (req, res) => {
   res.send({ title: 'Express' });
 });
 
 // GET METHOD
-defaultRoute.get('/product', (req, res) => {
-  res.send({ title: 'GET: Products' });
-});
 defaultRoute.get('/services', (req, res) => {
-  res.send({ title: 'GET: Services' });
+  connection.query('SELECT * FROM servicios', (error, results, fields) => {
+    if (error) {
+      res.status(500).send('Error executing the query');
+      console.error(error);
+      return;
+    }
+    res.json(results);
+  });
 });
 defaultRoute.get('/clients', (req, res) => {
-  res.send({ title: 'GET: Clients' });
+  connection.query('SELECT * FROM usuarios', (error, results, fields) => {
+    if (error) {
+      res.status(500).send('Error executing the query');
+      console.error(error);
+      return;
+    }
+    res.json(results);
+  });
 });
 // GET METHOD
 
 
 // POST METHOD
 defaultRoute.post('/clients', (req, res) => {
+  connection.query('INSERT INTO usuarios SET ?', req.body, (err, results) => {
+  if (err) {
+    console.error('Error al insertar usuario: ' + err.message);
+  res.status(404).send('POST: Error')
+    return;
+  }
+  console.log('Nuevo usuario insertado con ID:', results.insertId);
+  res.status(200).send('POST: Success')
+});
   console.log('POST: Clientes que recibo: ', JSON.stringify(req.body))
   res.status(200).send('POST: Success')
 });
-defaultRoute.post('/product', (req, res) => {
-  console.log('POST: Productos que recibo: ', JSON.stringify(req.body))
+
+
+defaultRoute.post('/services', (req, res) => {
+  connection.query('INSERT INTO servicios SET ?', req.body, (err, results) => {
+  if (err) {
+    console.error('Error al insertar usuario: ' + err.message);
+  res.status(404).send('POST: Error')
+    return;
+  }
+  console.log('Nuevo usuario insertado con ID:', results.insertId);
   res.status(200).send('POST: Success')
 });
-defaultRoute.post('/services', (req, res) => {
   console.log('POST: Services que recibo: ', JSON.stringify(req.body))
-  res.status(200).send('POST: Success')
 });
 // POST METHOD
 
@@ -58,15 +84,27 @@ defaultRoute.put('/services', (req, res) => {
 
 // DELETE METHOD
 defaultRoute.delete('/clients', (req, res) => {
-  console.log('DELETE: Clientes que recibo: ', JSON.stringify(req.body))
+  connection.query('DELETE FROM usuarios WHERE id = ?', req.query.id, (err, results) => {
+  if (err) {
+    console.error('Error al eliminar usuario: ' + err.message);
+    return;
+  }
+  console.log('Usuario eliminado correctamente');
+});
+  console.log('DELETE: Clientes que recibo: ', JSON.stringify(req.query.id))
   res.status(200).send('DELETE: Success')
 });
-defaultRoute.delete('/product', (req, res) => {
-  console.log('DELETE: Productos que recibo: ', JSON.stringify(req.body))
-  res.status(200).send('DELETE: Success')
-});
+
+
 defaultRoute.delete('/services', (req, res) => {
-  console.log('DELETE: Services que recibo: ', JSON.stringify(req.body))
+  connection.query('DELETE FROM servicios WHERE id = ?', req.query.id, (err, results) => {
+  if (err) {
+    console.error('Error al eliminar usuario: ' + err.message);
+    return;
+  }
+  console.log('Usuario eliminado correctamente');
+});
+  console.log('DELETE: Services que recibo: ', JSON.stringify(req.query.id))
   res.status(200).send('DELETE: Success')
 });
 // DELETE METHOD
